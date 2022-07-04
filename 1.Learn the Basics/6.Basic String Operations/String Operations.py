d548be12730ec5c57ac63c8cab96874ba8eb0a25
wayne <wayne20051211@gmail.com>
2022-07-04 16:38:21 +0800

    upload_4

--- 1.Learn the Basics/6.Basic String Operations/String Operations.py
+++ 1.Learn the Basics/6.Basic String Operations/String Operations.py
@@ -2,3 +2,8 @@ string = "Hello World!"
 print(string[3:7]) #part of string
 print(string[3:10:2]) #part of string
 print(string[::-1]) #reverse
+print(string.upper()) #upper
+print(string.lower()) #lower
+print(string.startswith("Hello")) #check the start of sentence(True or False)
+print(string.endswith("python")) #check the end of sentence(True or False)
+print(string.split(" ")) #split sentence with SPACE
